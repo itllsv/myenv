6f2d19a6d9fabadedbf870cacf8bc525eeb6a37b
Itallo Sá Vieira <itllsv@Itallos-MacBook-Pro.local>
2025-01-17 11:20:11 -0300

    feat: add nvim dots

---
+++ dotfiles/n/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+}
